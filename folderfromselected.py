5565c81a79302d6ed1cfa51db94c9e1bd68a9868
luizoti <luizlazoti@hotmail.com>
2020-08-05 14:52:37 -0300

    new windows structure inplement

--- folderfromselected.py
+++ folderfromselected.py
@@ -1,23 +1,59 @@
 #!/usr/bin/python3
 
-import os
 import sys
-import os.path
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from os.path import basename
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QListWidget, QVBoxLayout
  
-class FileList(QWidget):
+class App(QWidget):
+
     def __init__(self):
-        self.App = QApplication(sys.argv)
-        self.App.size(200, 200)
-        self.label = QLabel("Hello Word")
-        self.label.show()
-        self.App.exec_()
+        super().__init__()
+        self.title = 'Criar Pasta'
+
+        self.width = 480
+        self.height = 680 - 40
+
+        self.left = round(int((size.width() - self.width) / 2))
+        self.top = round(int((size.height() - self.height) / 2))
+        self.initUI()
+
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left,
+                         self.top, 
+                         self.width, 
+                         self.height)
+
+        vbox= QVBoxLayout()
+        self.list = QListWidget()
+
+        count = 0
+
+        for filename in categorize(sys.argv):
+            if not sys.argv[0] == filename:
+                self.list.insertItem(count, basename(filename))
+                count + 1
+                pass
+            pass
+
+        self.list.clicked.connect(self.listview_clicked)
+        vbox.addWidget(self.list)
+        self.label = QLabel()
+        self.label.setFont(QtGui.QFont("Sanserif", 15))
+        vbox.addWidget(self.label)
+        self.setLayout(vbox)
+        self.show()
+
+
+    def listview_clicked(self):
+        item = self.list.currentItem()
+        self.label.setText(str(item.text()))
+
 
 # def makelist(self):
-#     for filename in sys.argv:
-#         if not sys.argv[0] == filename:
-#             pass
-#         pass
+
 
 # # def editname(self):
 
@@ -29,5 +65,10 @@ class FileList(QWidget):
   
 
 if __name__ == '__main__':
-    FileList()
+    app = QApplication(sys.argv)
+    screen = app.primaryScreen()
+    size = screen.size()
+
+    ex = App()
+    sys.exit(app.exec_())
     
