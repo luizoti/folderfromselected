85062e6ad9b1d7f71473653c17201c13084d239d
luizoti <luizlazoti@hotmail.com>
2020-08-06 13:02:38 -0300

    base layout, capture only filename

--- folderfromselected.py
+++ folderfromselected.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 import sys
-from os.path import basename
+from os.path import basename, splitext
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QListWidget, QVBoxLayout
+from PyQt5.QtWidgets import QPushButton, QWidget, QApplication, QLabel, QListWidget, QHBoxLayout, QVBoxLayout, QLineEdit
  
 class App(QWidget):
 
@@ -11,45 +11,50 @@ class App(QWidget):
         super().__init__()
         self.title = 'Criar Pasta'
 
-        self.width = 480
-        self.height = 680 - 40
+        self.width = 800
+        self.height = 400
 
-        self.left = round(int((size.width() - self.width) / 2))
-        self.top = round(int((size.height() - self.height) / 2))
+        self.left = round(int((size.width() - self.width)/ 2))
+        self.top = round(int((size.height() - self.height + 40) / 2))
+        # 
+        self.vbox = QVBoxLayout()
+        self.hbox = QHBoxLayout()
+        self.list = QListWidget()
         self.initUI()
 
 
     def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left,
-                         self.top, 
-                         self.width, 
-                         self.height)
-
-        vbox= QVBoxLayout()
-        self.list = QListWidget()
-
         count = 0
-
-        for filename in categorize(sys.argv):
+        for filename in sys.argv:
             if not sys.argv[0] == filename:
-                self.list.insertItem(count, basename(filename))
+                self.list.insertItem(count, basename(filename).split('.')[0])
                 count + 1
                 pass
             pass
 
         self.list.clicked.connect(self.listview_clicked)
-        vbox.addWidget(self.list)
-        self.label = QLabel()
-        self.label.setFont(QtGui.QFont("Sanserif", 15))
-        vbox.addWidget(self.label)
-        self.setLayout(vbox)
+        self.vbox.addWidget(self.list)
+
+        self.button = QPushButton('Criar Pasta!')
+        self.textbox = QLineEdit()
+        self.pasta = QLabel('Pasta:')
+        self.hbox.addWidget(self.textbox)
+        self.hbox.addSpacing(10)
+        self.hbox.addWidget(self.button)
+
+        self.vbox.addLayout(self.hbox)
+        self.vbox.addWidget(self.pasta)
+        self.setLayout(self.vbox)
+        
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
 
 
     def listview_clicked(self):
         item = self.list.currentItem()
-        self.label.setText(str(item.text()))
+        textboxValue = str(item.text())
+        self.textbox.setText(textboxValue)
 
 
 # def makelist(self):
