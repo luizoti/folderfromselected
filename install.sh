008087cd80bfe2de8ca018ef10ecfc1f98ac9628
luiz <luizlazoti@hotmail.com>
2020-10-18 17:38:08 -0300

    install python, pip, pyqt5 and fix logic

--- install.sh
+++ install.sh
@@ -1,19 +1,50 @@
 #!/usr/bin/bash
 # 
+if [[ ! -f $(which python3) ]]; then
+    echo 
+    echo "_____________ Instalando Python3 _____________"
+    sudo apt-get update -y 
+    sudo apt-get install python3 -y
+fi
+
+GETPIP=/tmp/get-pip.py
+        
+if [[ ! -f ${GETPIP} ]]; then
+    echo 
+    echo "_____________ Instalando PIP _____________"
+    curl https://bootstrap.pypa.io/get-pip.py -o ${GETPIP}
+fi
+
+if sudo python3 ${GETPIP}; then
+    echo "Sucesso ao instalar PIP"
+else
+    echo "Erro ao instalar PIP"
+fi
+
+echo 
+echo "_____________ Instalando pyqt5 _____________"
+if sudo pip3 install pyqt5; then
+    echo "Sucesso ao instalar pyqt5"
+else
+    echo "Erro ao instalar pyqt5"
+fi
+
 USER=$(id -un 1000)
 DIR=$(dirname "$(readlink -f "$0")")
 SERVICE_DIR=/home/${USER}/.local/share/kservices5/ServiceMenus
 SERVICE="${DIR}/folderfromselected.desktop"
 
 SCRIPT_DIR=/home/${USER}/.config/folderfromselected
-SCRIPT="${DIR}/folderfromselected.py"
-ICOS="${DIR}/icos"
 
+icoold="Icon=.*"
+iconew="Icon=${DIR}/icos/folder.png"
 
 function install(){
     if [[ ! -d ${SERVICE_DIR} ]]; then
         sudo mkdir -p ${SERVICE_DIR}
-    else
+    fi
+
+    if sed -i "s|$icoold|$iconew|g" "${DIR}/folderfromselected.desktop"; then
         if [[ -f ${SERVICE} ]]; then
             if sudo cp -f ${SERVICE} ${SERVICE_DIR}; then
                 echo "folderfromselected.desktop, copiado com sucesso!"
@@ -22,19 +53,20 @@ function install(){
             fi
         fi
     fi
+
     echo
     if [[ ! -d ${SCRIPT_DIR} ]]; then
+        echo
+        echo "_____________ Baixando folderfromselected _____________"
         sudo -u ${USER} git clone 'https://github.com/luizoti/folderfromselected.git' ${SCRIPT_DIR}
     else
+        echo
+        echo "_____________ Atualizando folderfromselected _____________"
         cd ${SCRIPT_DIR}
         sudo -u ${USER} git pull origin master
     fi
 }
 
-if [[ -f "/usr/bin/plasma_session" ]] && [[ ${DESKTOP_SESSION} == "plasma" ]]; then
-    install
-else
-    echo "Esse escript foi escrito para o KDE, caso esteja usando outro Desktop Enviroment, instale manualmente."
-fi
+install
 
 exit
